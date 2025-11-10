8e2461f6844d870514945750fc31bb56425f9c00
Davishiio <dtdavid@gmail.com>
2025-11-10 02:23:08 -0500

    se añaden metodos para obtener data

--- src/services/convocatorias.api.js
+++ src/services/convocatorias.api.js
@@ -6,7 +6,11 @@ export const fetchCategorias = async () => {
     const { data } = await api.get('/categorias')
     return data
 }
-
+export const fetchAllConvocatoriasSummary = async () => {
+    const { data } = await api.get('/convocatorias/allConvocatorias')
+    // data = { categorias: [...], totalCategorias: N, totalConvocatorias: M }
+    return data
+}
 export const fetchConvocatoriasActivasByCategoria = async (idCategoria) => {
     // GET /api/convocatorias/:idCategoria/convocatorias-activas
     const { data } = await api.get(`/convocatorias/${idCategoria}/convocatorias-activas`)
@@ -21,3 +25,8 @@ export const fetchConvocatoriaDetalle = async (idCategoria, idConvocatoria) => {
     const { data } = await api.get(`/convocatorias/${idCategoria}/convocatoria/${idConvocatoria}`)
     return data
 }
+export const createCategoria = async (payload) => {
+    // payload: { nombreCategoria, descripcionCategoria, icono, orden }
+    const { data } = await api.post('/categorias', payload);
+    return data;
+};
